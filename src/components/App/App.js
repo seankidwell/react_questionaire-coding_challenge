5c965b7c18175bc2f0a86b2cebde43999b000dbd
seankidwell <dev.seankidwell@gmail.com>
2018-10-16 16:33:23 -0600

    finished

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -10,6 +10,7 @@ class App extends Component {
       list: []
     }
     this.handleChange = this.handleChange.bind(this);
+    this.deleteFrom = this.deleteForm.bind(this);
     this.submit = this.submit.bind(this);
   }
 
@@ -17,26 +18,38 @@ class App extends Component {
     this.setState({[event.target.name]: event.target.value});
   };
 
+  deleteForm(index) {
+    let list = this.state.list;
+    list.splice(index,1);
+    this.setState({list: list});
+  }
+
   submit(e) {
     e.preventDefault();
     this.setState({list: [this.state.userInput, ...this.state.list]});
-    this.setState({userInput: ''})
-    console.log(this.state.userInput, this.state.list);
-  }
+    this.setState({userInput: ''});
+  };
 
   render() {
 
-    let items = this.state.list.map((item, i) => <Form info={item}/>)
+    let items = this.state.list.map((item, i) => {
+      return (
+        <div key={i} className='wholeForm'>
+          <Form info={item}/>
+          <button onClick={() => this.deleteForm(i)}>Delete</button>
+        </div>
+      )
+    })
 
     return (
       <div className='App'>
+        <h1>Form App</h1>
         <form onSubmit={this.submit}>
-          <input type='text' name='userInput' value={this.state.userInput} onChange={this.handleChange}/>
+          <input type='text' name='userInput' placeholder='Type Here' value={this.state.userInput} onChange={this.handleChange}/>
           <button>Submit</button>
         </form>
         <div className='list'>
           {items}
-          {/* {this.state.list} */}
         </div>
       </div>
     );
